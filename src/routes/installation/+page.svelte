d1ebca164fcb4bbe3d1442d9990fa3f3e324856b
michellejanay <mindyourphotosynthesis@gmail.com>
2023-04-24 16:38:52 +0100

    feat(PB-34): page provides future link to extension with env

--- src/routes/installation/+page.svelte
+++ src/routes/installation/+page.svelte
@@ -1,14 +1,18 @@
 <script>
+	// import { env } from '$env/static/public';
+
 	function handleClick() {
-		alert('clicked!');
+		console.log(import.meta.env.VITE_INSTALLATION_LINK);
 	}
 </script>
 
 <main class="container h-full mx-auto flex justify-center items-center">
 	<section class="card p-6 bg-white shadow-lg">
 		<h3 class="m-4">Please click below to install the extension...</h3>
-		<button type="button" class="btn variant-filled mt-4 float-right" on:click={handleClick}
-			>Install Extension</button
+		<a
+			type="button"
+			class="btn variant-filled mt-4 float-right"
+			href={import.meta.env.VITE_INSTALLATION_LINK}>Install Extension</a
 		>
 	</section>
 </main>
