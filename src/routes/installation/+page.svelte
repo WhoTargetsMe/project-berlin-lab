d945a67cdea25c305f9b65f14746d9feb7e0e628
michellejanay <mindyourphotosynthesis@gmail.com>
2023-04-24 13:53:15 +0100

    feat(PB-34): extension installation page created

---
+++ src/routes/installation/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	function handleClick() {
+		alert('clicked!');
+	}
+</script>
+
+<main class="container h-full mx-auto flex justify-center items-center">
+	<section class="card p-6 bg-white shadow-lg">
+		<h3 class="m-4">Please click below to install the extension...</h3>
+		<button type="button" class="btn variant-filled mt-4 float-right" on:click={handleClick}
+			>Install Extension</button
+		>
+	</section>
+</main>
