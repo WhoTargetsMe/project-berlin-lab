11d7491984da80e1bb9dcad38ce8c2b2e9c1564a
Chidimma <chidimmamofodum@gmail.com>
2023-04-25 03:20:37 +0100

    fix(PB-37):setting success route to installation page

--- src/routes/landing/+page.svelte
+++ src/routes/landing/+page.svelte
@@ -1,21 +1,26 @@
 <script>
-  export let data;
-  import { onMount } from "svelte";
-  import { goto } from "$app/navigation";
+	export let data;
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
-  const keys = Object.values(data);
+	const keys = Object.values(data);
+	const hasParams = !keys.includes(null);
 
-  onMount(() => {
-    if (keys.includes(null)) {
-      setTimeout(() => {
-        window.location.href = "https://www.prolific.co"
-      }, 3000);
-    } else {
-      goto('/');
-    }
-  })
+	onMount(() => {
+		if (hasParams) {
+			goto('/installation');
+		} else {
+			setTimeout(() => {
+				window.location.href = 'https://www.prolific.co';
+			}, 5000);
+		}
+	});
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">Complete the onboarding process in prolific to continue. Redirecting in few seconds</div>
-</div>
+{#if !hasParams}
+	<div class="container h-full mx-auto flex justify-center items-center">
+		<div class="space-y-5">
+			Complete the onboarding process in prolific to continue. Redirecting in few seconds
+		</div>
+	</div>
+{/if}
