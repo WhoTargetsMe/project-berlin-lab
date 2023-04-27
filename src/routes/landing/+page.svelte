0fbad2ef3300c4bb3ed8956a9c73ee8cd0edeb03
Chidimma <chidimmamofodum@gmail.com>
2023-04-27 16:14:24 +0100

    using a signpost instead of redirecting

--- src/routes/landing/+page.svelte
+++ src/routes/landing/+page.svelte
@@ -1,32 +1,29 @@
 <script>
 	export let data;
 	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
 	import { PUBLIC_PROLIFIC_LINK } from '$env/static/public';
 
 	const keys = Object.values(data);
 	const hasProlificParams = !keys.includes(null);
 
 	onMount(() => {
-		if (!hasProlificParams) {
-			setTimeout(() => {
-				window.location.href = PUBLIC_PROLIFIC_LINK;
-			}, 5000);
-			return;
+		if (hasProlificParams) {
+			localStorage.clear();
+			window.localStorage.setItem('prolific_params', JSON.stringify(data));
+			//checks if user has extension
 		}
-		localStorage.clear();
-		window.localStorage.setItem('prolific params', JSON.stringify(data));
-
-		//checks if user has extension
-		// no - redirects to install extension
-		// otherwise
 	});
 </script>
 
 {#if !hasProlificParams}
-	<div class="container h-full mx-auto flex justify-center items-center">
-		<div class="space-y-5">
-			Complete the onboarding process in prolific to continue. Redirecting in few seconds
-		</div>
-	</div>
+	<main class="container h-full mx-auto flex justify-center items-center">
+		<section class="card p-6 bg-white shadow-lg">
+			<h3 class="m-4">
+				Please click on the button below to complete the onboarding process in prolific.
+			</h3>
+			<a type="button" class="btn variant-filled mt-4 float-right" href={PUBLIC_PROLIFIC_LINK}>
+				Go to Prolific
+			</a>
+		</section>
+	</main>
 {/if}
