f946beae0a314714623b9f166b5c359de27eb943
michellejanay <mindyourphotosynthesis@gmail.com>
2023-04-28 08:10:36 +0100

    feat(PB-35): checks installed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,16 @@
 <script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		let hasExtension = false;
+		const isInstalled = document.querySelectorAll('[src*="test.js"]');
+
+		if (isInstalled.length >= 1) {
+			hasExtension = true;
+		} else {
+			hasExtension = false;
+		}
+	});
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
