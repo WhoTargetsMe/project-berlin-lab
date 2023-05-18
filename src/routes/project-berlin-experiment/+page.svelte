130a8b8e0577670a87c2d9d2e966fa89a21bae84
Kevin Carmody <kevin@skinofstars.com>
2023-05-18 22:58:47 +0100

    refactor: move post types data to component

--- src/routes/project-berlin-experiment/+page.svelte
+++ src/routes/project-berlin-experiment/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-	import { onMount } from 'svelte';
-	import OrganicImagePost from '../../components/card/OrganicImagePost.svelte';
-	import OrganicPostNoImage from '../../components/card/OrganicPostNoImage.svelte';
+	import SponsoredPost from '../../components/SponsoredPost.svelte';
+	import OrganicPost from '../../components/OrganicPost.svelte';
+	import EngagementPost from '../../components/EngagementPost.svelte';
+	import Shorts from '../../components/Shorts.svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -12,14 +13,6 @@
 		} else {
 			return post.category;
 		}
-	};
-
-	const isOrganicRepost = (post) => {
-		return post.node.attached_story !== null;
-	};
-
-	const isOrganicNoImagePost = (post) => {
-		return post.node.content?.story.attachments.length === 0;
 	};
 </script>
 
@@ -33,91 +26,21 @@
 		</h1>
 	</div>
 
-	{#each data.posts.posts as item}
-		{#if getPostType(item) === 'ENGAGEMENT'}
-			<div class="card p-4 m-4 bg-white">
-				<p>Engagement post</p>
-			</div>
-		{:else if getPostType(item) === 'SPONSORED'}
-			<div class="card p-4 m-4 bg-white">
-				<p>Sponsored post</p>
-			</div>
-		{:else if getPostType(item) === 'ORGANIC' && isOrganicRepost(item)}
-			<OrganicImagePost
-				avatarSrc={item.node.comet_sections?.content.story.comet_sections.context_layout?.story
-					.comet_sections.actor_photo?.story.actors[0].profile_picture.uri}
-				avatarAlt={item.node.comet_sections.content.story.comet_sections.context_layout?.story
-					.comet_sections.actor_photo.story.actors[0].profile_uri}
-				posterName={item.node.comet_sections.content.story.comet_sections.context_layout?.story
-					.comet_sections.actor_photo.story.actors[0].name}
-				timePosted={item.node.comet_sections.context_layout.story.comet_sections.metadata[1].story
-					.creation_time}
-				postMessage={item.node.comet_sections.content?.story.message?.text}
-				postImage={item.node.comet_sections.content.story.attachments[0]?.styles.attachment.media
-					?.photo_image?.uri}
-				postSrc={item.node.comet_sections.content.story.attachments[0]?.styles.attachment.media
-					?.photo_image?.uri}
-				postAlt={item.node.comet_sections.content.story?.attachments[0]?.styles.attachment.media
-					?.accessibility_caption}
-				totalReactions={item.node.comet_sections.feedback.story.feedback_context
-					.feedback_target_with_context.ufi_renderer.feedback.comet_ufi_summary_and_actions_renderer
-					.feedback.i18n_reaction_count}
-				totalComments={item.node.comet_sections.feedback.story.feedback_context
-					.feedback_target_with_context.ufi_renderer.feedback.total_comment_count}
-				totalShares={item.node.comet_sections.feedback.story.feedback_context
-					.feedback_target_with_context.ufi_renderer.feedback.comet_ufi_summary_and_actions_renderer
-					.feedback.i18n_share_count}
-			/>
-		{:else if getPostType(item) === 'ORGANIC' && isOrganicNoImagePost(item)}
-			<OrganicPostNoImage
-				avatarSrc={item.node.comet_sections?.content.story.comet_sections.context_layout?.story
-					.comet_sections.actor_photo?.story.actors[0].profile_picture.uri}
-				avatarAlt={item.node.comet_sections.content.story.comet_sections.context_layout?.story
-					.comet_sections.actor_photo.story.actors[0].profile_uri}
-				posterName={item.node.comet_sections.content.story.comet_sections.context_layout?.story
-					.comet_sections.actor_photo.story.actors[0].name}
-				timePosted={item.node.comet_sections.context_layout.story.comet_sections.metadata[1].story
-					.creation_time}
-				postMessage={item.node.comet_sections.content?.story.message.text}
-				totalReactions={item.node.comet_sections.feedback.story.feedback_context
-					.feedback_target_with_context.ufi_renderer.feedback.comet_ufi_summary_and_actions_renderer
-					.feedback.i18n_reaction_count}
-				totalComments={item.node.comet_sections.feedback.story.feedback_context
-					.feedback_target_with_context.ufi_renderer.feedback.total_comment_count}
-				totalShares={item.node.comet_sections.feedback.story.feedback_context
-					.feedback_target_with_context.ufi_renderer.feedback.comet_ufi_summary_and_actions_renderer
-					.feedback.i18n_share_count}
-			/>
-		{:else if getPostType(item) === 'ORGANIC' && !isOrganicRepost(item) && !isOrganicNoImagePost(item)}
-			<OrganicImagePost
-				avatarSrc={item.node.comet_sections?.content.story.comet_sections.context_layout?.story
-					.comet_sections.actor_photo?.story.actors[0].profile_picture.uri}
-				avatarAlt={item.node.comet_sections.content.story.comet_sections.context_layout?.story
-					.comet_sections.actor_photo.story.actors[0].profile_uri}
-				posterName={item.node.comet_sections.content.story.comet_sections.context_layout?.story
-					.comet_sections.actor_photo.story.actors[0].name}
-				timePosted={item.node.comet_sections.context_layout.story.comet_sections.metadata[1].story
-					.creation_time}
-				postMessage={item.node.comet_sections.content?.story.message?.text}
-				postImage={item.node.comet_sections.content.story.attachments[0]?.styles.attachment.media
-					?.photo_image?.uri}
-				postSrc={item.node.comet_sections.content.story.attachments[0]?.styles.attachment.media
-					?.photo_image?.uri}
-				postAlt={item.node.comet_sections.content.story?.attachments[0]?.styles.attachment.media
-					?.accessibility_caption}
-				totalReactions={item.node.comet_sections.feedback.story.feedback_context
-					.feedback_target_with_context.ufi_renderer.feedback.comet_ufi_summary_and_actions_renderer
-					.feedback.i18n_reaction_count}
-				totalComments={item.node.comet_sections.feedback.story.feedback_context
-					.feedback_target_with_context.ufi_renderer.feedback.total_comment_count}
-				totalShares={item.node.comet_sections.feedback.story.feedback_context
-					.feedback_target_with_context.ufi_renderer.feedback.comet_ufi_summary_and_actions_renderer
-					.feedback.i18n_share_count}
-			/>
-		{:else}
-			<div class="card p-4 m-4 bg-white">
-				<p>Facebook Shorts</p>
-			</div>
-		{/if}
-	{/each}
+	{#if data.posts.posts}
+		{#each data.posts.posts as post}
+			{#if getPostType(post) === 'ENGAGEMENT'}
+				<EngagementPost {post} />
+			{:else if getPostType(post) === 'SPONSORED'}
+				<SponsoredPost {post} />
+			{:else if getPostType(post) === 'ORGANIC'}
+				<OrganicPost {post} />
+			{:else}
+				<Shorts {post} />
+			{/if}
+		{/each}
+	{:else}
+		<div class="card p-4 m-4">
+			<p>Please collect some facebook posts</p>
+		</div>
+	{/if}
 </main>
