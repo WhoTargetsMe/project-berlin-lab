5d7e52cee096007a76a676eae7e809f83197a674
Kevin Carmody <kevin@skinofstars.com>
2023-05-22 16:43:11 +0100

    feat(PB-61): set up Posthog with general event tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,17 @@
-<script lang='ts'>
+<script lang="ts">
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/all.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+	import posthog from 'posthog-js';
+
+	// Because SvelteKit is an SPA, we need to manually record navigation events
+	$: $page.url.pathname, browser && posthog.capture('$pageview');
 </script>
 
 <slot />
