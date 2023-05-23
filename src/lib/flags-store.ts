2bb9f233bc820a7cdabde58a684c550872f3688d
michellejanay <mindyourphotosynthesis@gmail.com>
2023-05-23 14:46:33 +0100

    feat: payload added to flags object

--- src/lib/flags-store.ts
+++ src/lib/flags-store.ts
@@ -6,6 +6,7 @@ export const flags = writable({});
 posthog.onFeatureFlags((phFlags) => {
 	const flagsObj = phFlags.reduce((result, item) => {
 		result[item] = item;
+		result['payload'] = posthog.getFeatureFlagPayload(item);
 		return result;
 	}, {});
 	flags.set(flagsObj);
