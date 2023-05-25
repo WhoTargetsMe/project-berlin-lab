d324f0e36af0ad488c21418dd03f584b331abca6
michellejanay <mindyourphotosynthesis@gmail.com>
2023-05-25 11:15:43 +0100

    feat: posthog custom event tracking added

---
+++ src/components/CallToAction.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { posthog } from 'posthog-js';
+
+	const clickedButton = () => {
+		posthog.capture('Call to Action button clicked', {
+			buttonClass: 'call-to-action',
+			buttonText: 'Click me :D'
+		});
+	};
+</script>
+
+<main>
+	<p>This is an example call to action</p>
+	<button
+		on:click={clickedButton}
+		class="call-to-action bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+		>Click me :D</button
+	>
+</main>
