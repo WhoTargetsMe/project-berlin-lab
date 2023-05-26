43230ab88a0d4ec52b4719bf7067832b76e8e1b0
michellejanay <mindyourphotosynthesis@gmail.com>
2023-05-26 11:42:13 +0100

    test: loading flags before doing things with them

--- src/lib/flags-store.ts
+++ src/lib/flags-store.ts
@@ -2,17 +2,17 @@ import { writable } from 'svelte/store';
 import posthog from 'posthog-js';
 export const flags = writable({});
 
-const posthogFlags: [string] = posthog.feature_flags.getFlags();
-const flagsWithPayload: { [key: string]: { enabled: boolean; payload: any } } = posthogFlags.reduce(
-	(result, item) => {
-		return {
-			...result,
-			[item]: {
-				enabled: posthog.getFeatureFlag(item),
-				payload: posthog.getFeatureFlagPayload(item)
-			}
-		};
-	},
-	{}
-);
-flags.set(flagsWithPayload);
+posthog.onFeatureFlags(() => {
+	const posthogFlags: [] = posthog.feature_flags.getFlags();
+	const flagsWithPayload: { [key: string]: { enabled: boolean; payload: any } } =
+		posthogFlags.reduce((result, item) => {
+			return {
+				...result,
+				[item]: {
+					enabled: posthog.getFeatureFlag(item),
+					payload: posthog.getFeatureFlagPayload(item)
+				}
+			};
+		}, {});
+	flags.set(flagsWithPayload);
+});
