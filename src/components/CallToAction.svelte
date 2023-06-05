edb30380bc9ba6235cf9c3667be91787e3da4909
michellejanay <mindyourphotosynthesis@gmail.com>
2023-06-05 15:49:20 +0100

    feat: multi subattachment card info added

--- src/components/CallToAction.svelte
+++ src/components/CallToAction.svelte
@@ -10,6 +10,9 @@
 
 	export let post;
 
+	const sponsoredURL =
+		post.node.comet_sections.content.story.attachments[0].styles.attachment.source?.text;
+
 	let formOnFacebookImage =
 		post.node?.comet_sections.content.story.attachments[0].comet_footer_renderer?.attachment
 			.call_to_action_renderer.action_link.media?.lead_gen_brand_image.uri;
@@ -28,12 +31,19 @@
 		post.node.comet_sections.content.story.attachments[0].comet_footer_renderer?.attachment
 			.call_to_action_renderer?.action_link.title;
 
-	let multipImage =
-		post.node.comet_sections.content.story.attachments[0].styles.attachment.subattachments?.map(
-			(attached) => attached.multi_share_media_card_renderer.attachment.media.image.uri
+	let multiBtnText =
+		post.node.comet_sections.content.story.attachments[0].styles.attachment.subattachments;
+	// let multipImage =
+	// 	post.node.comet_sections.content.story.attachments[0].styles.attachment.subattachments?.map(
+	// 		(attached) => attached.multi_share_media_card_renderer.attachment.media.image.uri
+	// 	);
+
+	const subAttach =
+		post.node.comet_sections.content.story?.attachments[0].styles.attachment.subattachments?.map(
+			(attached) => attached
 		);
 
-	console.log(multipImage);
+	// console.log(cardText);
 </script>
 
 <div class="bg-slate-100 border mx-0 px-0 flow-root">
@@ -45,19 +55,27 @@
 		>
 	{/if}
 
-	{#if multipImage}
+	{#if subAttach}
 		<div class="carousel carousel-center p-4 bg-white" data-carousel="static">
 			<div class="relative rounded-lg flex overflow-x-scroll">
-				{#each multipImage as image, i}
+				{#each subAttach as sub, i}
 					<div
 						id={JSON.stringify(i)}
-						class="carousel-item border border-rounded mx-4 data-carousel-item"
+						class="carousel-item border rounded-lg mx-4 data-carousel-item"
 					>
-						<img src={image} alt="sponsored-multi" />
-						<button
-							class="bg-gray-200 hover:bg-gray-300 font-medium my-5 mx-4 py-2 px-4 rounded float-right"
-							>{cTAText || otherCTAText}</button
-						>
+						<img
+							src={sub.multi_share_media_card_renderer.attachment.media.image.uri}
+							alt="sponsored-multi"
+						/>
+						<div>
+							<p>{sponsoredURL}</p>
+							<p>{sub.card_title.text}</p>
+
+							<button
+								class="bg-gray-200 hover:bg-gray-300 font-medium my-5 mx-4 py-2 px-4 rounded float-right"
+								>{sub.call_to_action_renderer.action_link.title}</button
+							>
+						</div>
 					</div>
 				{/each}
 			</div>
