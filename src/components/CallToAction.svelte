5396d44e3a7558e6c3b7c0fd8f6aebcbde2b6d7e
michellejanay <mindyourphotosynthesis@gmail.com>
2023-06-05 16:46:36 +0100

    feat: call to action card details added

--- src/components/CallToAction.svelte
+++ src/components/CallToAction.svelte
@@ -13,26 +13,35 @@
 	const sponsoredURL =
 		post.node.comet_sections.content.story.attachments[0].styles.attachment.source?.text;
 
-	let formOnFacebookImage =
+	const formOnFacebookImage =
 		post.node?.comet_sections.content.story.attachments[0].comet_footer_renderer?.attachment
-			.call_to_action_renderer.action_link.media?.lead_gen_brand_image.uri;
+			.call_to_action_renderer?.action_link.media?.lead_gen_brand_image.uri;
 
-	let otherSponsoredImage =
+	const otherSponsoredImage =
 		post.node.comet_sections?.content.story.attachments[0].styles.attachment.media.large_share_image
 			?.uri;
 
+	const sponsorURL =
+		post.node.comet_sections.content.story.attachments[0].comet_footer_renderer?.attachment.source
+			.text;
+
+	const sponsorTitle =
+		post.node.comet_sections.content.story.attachments[0].styles?.attachment.title_with_entities
+			?.text;
+
+	const sponsorDescription =
+		post.node.comet_sections.content.story.attachments[0].comet_footer_renderer?.attachment
+			.description.text;
 	// let buttonText = post.node?.comet_sections.content.story.attachements?[0].comet_footer_renderer?.attachment.call_to_action_renderer.action_link.stateful_title;
 
-	let cTAText =
+	const cTAText =
 		post.node.comet_sections.content.story.attachments[0].comet_footer_renderer?.attachment
-			.call_to_action_renderer.action_link.stateful_title;
+			.call_to_action_renderer?.action_link.stateful_title;
 
-	let otherCTAText =
+	const otherCTAText =
 		post.node.comet_sections.content.story.attachments[0].comet_footer_renderer?.attachment
 			.call_to_action_renderer?.action_link.title;
 
-	let multiBtnText =
-		post.node.comet_sections.content.story.attachments[0].styles.attachment.subattachments;
 	// let multipImage =
 	// 	post.node.comet_sections.content.story.attachments[0].styles.attachment.subattachments?.map(
 	// 		(attached) => attached.multi_share_media_card_renderer.attachment.media.image.uri
@@ -49,10 +58,17 @@
 <div class="bg-slate-100 border mx-0 px-0 flow-root">
 	{#if formOnFacebookImage || otherSponsoredImage}
 		<img class="p-0 m-0" src={formOnFacebookImage || otherSponsoredImage} alt="sponsored" />
-		<button
-			class="bg-gray-200 hover:bg-gray-300 font-medium my-5 mx-4 py-2 px-4 rounded float-right"
-			>{cTAText || otherCTAText}</button
-		>
+		<div>
+			<section class="px-4 pt-2">
+				<p class="font-light text-slate-700">{sponsorURL.toUpperCase()}</p>
+				<p><strong>{sponsorTitle}</strong></p>
+				<p class="font-light text-slate-700">{sponsorDescription}</p>
+			</section>
+			<button
+				class="bg-gray-200 hover:bg-gray-300 font-medium my-5 mx-4 py-2 px-4 rounded float-right"
+				>{cTAText || otherCTAText}</button
+			>
+		</div>
 	{/if}
 
 	{#if subAttach}
@@ -68,7 +84,7 @@
 							alt="sponsored-multi"
 						/>
 						<div>
-							<p>{sponsoredURL}</p>
+							<!-- <p>{sponsoredURL}</p> -->
 							<p>{sub.card_title.text}</p>
 
 							<button
