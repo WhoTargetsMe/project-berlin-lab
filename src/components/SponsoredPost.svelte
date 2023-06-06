34adc081a582db4e9727d310428a5ab470b5ce38
michellejanay <mindyourphotosynthesis@gmail.com>
2023-06-06 11:57:21 +0100

    refactor: comments removed

--- src/components/SponsoredPost.svelte
+++ src/components/SponsoredPost.svelte
@@ -3,28 +3,9 @@
 	import CallToAction from './CallToAction.svelte';
 	import LikeCommentButtons from './LikeCommentButtons.svelte';
 	import PosterName from './PosterName.svelte';
-	import PostImage from './PostImage.svelte';
 	import PostMessage from './PostMessage.svelte';
 	import Reactions from './Reactions.svelte';
 	export let post = {};
-
-	let postMessage = post.node.comet_sections.content.story.message.text;
-
-	console.log(post);
-
-	const lineClampText = document.querySelectorAll('.line-clamp-2');
-	const readMoretext = document.querySelectorAll('.read-more-text');
-
-	// const isTextClamped = (text) =>
-	// 	text.scrollHeight > text.clientHeight || text.offsetWidth < text.scrollWidth;
-
-	const readMore = () =>
-		readMoretext.forEach((seeMore) => {
-			seeMore.addEventListener('click', () => {
-				lineClampText?.classList.remove('line-clamp-2');
-				readMoretext.textContent = '';
-			});
-		});
 </script>
 
 <main class="card my-4 bg-white shadow-sm">
