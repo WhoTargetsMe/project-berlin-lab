57dac4406e40aa10c82ddef3e6682bb5a8cb9cec
michellejanay <mindyourphotosynthesis@gmail.com>
2023-06-08 10:07:02 +0100

    feat: random, sort by post type, removing sponsored feature flags added

--- src/routes/project-berlin-experiment/+page.svelte
+++ src/routes/project-berlin-experiment/+page.svelte
@@ -5,6 +5,7 @@
 	import Repost from '../../components/Repost.svelte';
 	import Shorts from '../../components/Shorts.svelte';
 	import { PUBLIC_TYPEFORM_LINK } from '$env/static/public';
+	import { flags } from '$lib/flags-store';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -23,6 +24,29 @@
 	const { prolific_pid, study_id, session_id, form_id } = data.prolificParams;
 
 	const offBoardLink = `${PUBLIC_TYPEFORM_LINK}/${form_id}#prolific_pid=${prolific_pid}&study_id=${study_id}&session_id=${session_id}&offboarding=${true}`;
+
+	const sortPosts = (category) => {
+		return data.posts.posts.sort((a, b) =>
+			a.category === category && b.category !== category ? -1 : 1
+		);
+	};
+
+	if ($flags.should_sort_random.enabled) {
+		data.posts.posts.sort(() => Math.random() - 0.5);
+	}
+	if ($flags.should_emphasize_organic_posts.enabled) {
+		sortPosts('ORGANIC');
+	}
+	if ($flags.should_emphasize_engagement_posts.enabled) {
+		sortPosts('ENGAGEMENT');
+	}
+	if ($flags.should_emphasize_sponsored_posts.enabled) {
+		sortPosts('SPONSORED');
+	}
+	if ($flags.should_not_show_sponsored.enabled) {
+		const filtered = data.posts.posts.filter((post) => post.category !== 'SPONSORED');
+		data.posts.posts = filtered;
+	}
 </script>
 
 <main>
