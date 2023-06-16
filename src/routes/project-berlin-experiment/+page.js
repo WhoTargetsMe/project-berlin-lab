df74b39c3980b45e5d0fba569c562ea348827605
michellejanay <mindyourphotosynthesis@gmail.com>
2023-06-16 09:43:31 +0100

    feat: redirection from the experiment page if no extension is installed

--- src/routes/project-berlin-experiment/+page.js
+++ src/routes/project-berlin-experiment/+page.js
@@ -1,8 +1,18 @@
 export const ssr = false;
 import { PUBLIC_EXTENSION_ID } from '$env/static/public';
+import { goto } from '$app/navigation';
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
+	// This only works in Chrome
+	if (!window.chrome || !window.chrome.runtime) {
+		goto('../installation');
+	} else {
+		window.chrome.runtime.sendMessage(PUBLIC_EXTENSION_ID, {
+			type: 'is_installed'
+		});
+	}
+
 	let postsRequest = new Promise((resolve, reject) => {
 		chrome.runtime.sendMessage(PUBLIC_EXTENSION_ID, { type: 'get_posts' }, (data) => {
 			if (data) {
