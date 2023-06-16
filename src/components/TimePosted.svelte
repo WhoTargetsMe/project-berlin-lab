f9a42642c6fd197be82b9f2b610b6b7e9d2b0e86
michellejanay <mindyourphotosynthesis@gmail.com>
2023-06-16 10:53:27 +0100

    fix: optional chaining added to time-posted

--- src/components/TimePosted.svelte
+++ src/components/TimePosted.svelte
@@ -4,7 +4,7 @@
 
 	//todo format time
 	let timePosted =
-		post.node.comet_sections.context_layout?.story.comet_sections.metadata[1].story.creation_time;
+		post.node.comet_sections.context_layout?.story.comet_sections.metadata[1]?.story.creation_time;
 
 	$: hasFlags = Object.keys($flags).length > 0;
 </script>
