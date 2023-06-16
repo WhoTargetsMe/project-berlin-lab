4bcf82d1ebcebf8f257a86136261b2819984864b
michellejanay <mindyourphotosynthesis@gmail.com>
2023-06-16 15:59:44 +0100

    feat: formatting time using date-fns

--- src/components/TimePosted.svelte
+++ src/components/TimePosted.svelte
@@ -1,16 +1,36 @@
 <script lang="ts">
 	export let post: Post = {};
 	import { flags } from '$lib/flags-store';
+	import { format, fromUnixTime, formatDistanceToNowStrict } from 'date-fns';
 
-	//todo format time
-	let timePosted =
-		post.node.comet_sections.context_layout?.story.comet_sections.metadata[1]?.story.creation_time;
+	//to fix: if it says 53years, it's the wrong date/time posted
+	const timePosted =
+		post.node.comet_sections.context_layout?.story.comet_sections.metadata[1]?.story
+			.creation_time || new Date(0);
+
+	const dateOfPost = fromUnixTime(timePosted);
+	const formattedDate = format(dateOfPost, 'dd MMM yyyy');
+	const formattedTimeDistance = formatDistanceToNowStrict(dateOfPost);
+
+	const time = () => {
+		let [n, frame] = formattedTimeDistance.split(' ');
+
+		if (frame === 'days' && parseInt(n) > 6) {
+			return formattedDate;
+		} else {
+			return formattedTimeDistance
+				.replaceAll(' ', '')
+				.replaceAll('hours', 'h')
+				.replaceAll('days', 'd')
+				.replaceAll('day', 'd');
+		}
+	};
 
 	$: hasFlags = Object.keys($flags).length > 0;
 </script>
 
 {#if hasFlags}
-	{#if $flags.should_display_time_posted.enabled}
-		<p>{timePosted}</p>
+	{#if $flags.should_display_time_posted}
+		<p>{time()}</p>
 	{/if}
 {/if}
