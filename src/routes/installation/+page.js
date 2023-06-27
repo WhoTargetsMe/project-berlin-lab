5164c889e3fbcff5e739ba8350eb239c63b404a6
Chidimma <chidimmamofodum@gmail.com>
2023-06-27 12:00:28 -0400

    posthog identity error fixed when extension is not installed

--- src/routes/installation/+page.js
+++ src/routes/installation/+page.js
@@ -5,11 +5,14 @@ export function load() {
 
 	const { prolific_pid, study_id, session_id, form_id } = prolific_params;
 	const hasProlificParams = prolific_pid && study_id && session_id;
+	const hasExtension = window.chrome && window.chrome.runtime;
+	const landingPageUrl = `/landing?prolific_pid=${prolific_pid}&study_id=${study_id}&session_id=${session_id}&form_id=${form_id}`;
 
-	if (window.chrome && window.chrome.runtime && hasProlificParams) {
-		window.location.href = `/landing?prolific_pid=${prolific_pid}&study_id=${study_id}&session_id=${session_id}&form_id=${form_id}`;
+	if (hasExtension && hasProlificParams) {
+		window.location.href = landingPageUrl;
+	} else if (!hasExtension) {
+		return;
 	} else if (!hasProlificParams) {
 		goto('../landing');
 	}
-	return {};
 }
