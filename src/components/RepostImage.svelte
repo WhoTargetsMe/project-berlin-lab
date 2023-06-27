dbed44fe7212a489c5bd78028435ac6daa10fb2e
michellejanay <mindyourphotosynthesis@gmail.com>
2023-06-27 11:57:30 +0100

    fix: jsonpath installed

--- src/components/RepostImage.svelte
+++ src/components/RepostImage.svelte
@@ -3,11 +3,9 @@
 	import { JSONPath } from 'jsonpath-plus';
 	export let post: Post = {};
 
-	let multipleImages = JSONPath({ path: '$..all_subattachments.nodes', json: post })[0];
-
-	if (multipleImages) {
-		multipleImages.map((img: { [key: string]: any }) => img.media.image.uri);
-	}
+	let multipleImages = JSONPath({ path: '$..all_subattachments.nodes', json: post })[0].map(
+		(img: { [key: string]: any }) => img.media.image.uri
+	);
 
 	let imageRepost = JSONPath({ path: '$..media.photo_image.uri', json: post })[0];
 </script>
