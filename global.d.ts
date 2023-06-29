5e64f9773706a2b5947d0c20ce30b9a665ebe218
Chidimma <chidimmamofodum@gmail.com>
2023-06-29 11:13:40 -0400

    removing the logo in the home page. already included in layout

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,3 @@
 type Post = {
 	[key: string]: any;
 };
-
-export interface Window {
-	[key: string]: any;
-}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,8 @@
 <script>
 </script>
 
-<main class="h-full">
-	<div class="w-12 h-12">
-		<img src="/wtm_logo_2020.png" alt="wtm logo" />
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		Check out the installation page <a href="./installation">Installation</a>
 	</div>
-
-	<div class="container h-full mx-auto flex justify-center items-center">
-		<div class="space-y-5">
-			Check out the installation page <a href="./installation">Installation</a>
-		</div>
-	</div>
-</main>
+</div>
