eb5fff07e15274a8a1b2e09f17dc3b7f301ad33d
Chidimma <chidimmamofodum@gmail.com>
2023-06-30 11:53:22 -0400

    (PB-91):feat/configure post collection count from lab

--- src/routes/wtm-experiment/+page.ts
+++ src/routes/wtm-experiment/+page.ts
@@ -1,5 +1,5 @@
 export const ssr = false;
-import { PUBLIC_EXTENSION_ID } from '$env/static/public';
+import { PUBLIC_EXTENSION_ID, PUBLIC_TARGET_POST_NUMBER } from '$env/static/public';
 import { goto } from '$app/navigation';
 
 /** @type {import('./$types').PageLoad} */
@@ -14,13 +14,17 @@ export async function load() {
 	}
 
 	let postsRequest = new Promise((resolve, reject) => {
-		chrome.runtime.sendMessage(PUBLIC_EXTENSION_ID, { type: 'get_posts' }, (data) => {
-			if (data) {
-				return resolve(data);
-			} else {
-				return reject('error message');
+		chrome.runtime.sendMessage(
+			PUBLIC_EXTENSION_ID,
+			{ type: 'get_posts', data: { postNum: parseInt(PUBLIC_TARGET_POST_NUMBER) } },
+			(data) => {
+				if (data) {
+					return resolve(data);
+				} else {
+					return reject('error message');
+				}
 			}
-		});
+		);
 	});
 
 	return {
