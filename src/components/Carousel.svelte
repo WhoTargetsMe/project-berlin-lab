152bc4594354fbe2f13650b627539d112917611f
michellejanay <mindyourphotosynthesis@gmail.com>
2023-07-07 14:01:46 +0100

    style: carousel of 2 images style updated and carousel with 5 images style update

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
 	export let multipleImages: [] = [];
+	console.log(multipleImages);
 </script>
 
-<div class="grid grid-cols-3">
-	{#each multipleImages as image}
-		<div class="col-span-1">
-			<img class="w-full" src={image} alt="organic post" />
-		</div>
-	{/each}
-</div>
+{#if multipleImages.length === 2}
+	<div class="grid grid-cols-2 gap-1">
+		{#each multipleImages as image}
+			<img class="w-fit" src={image} alt="organic post" />
+		{/each}
+	</div>
+{/if}
+
+{#if multipleImages.length === 3}
+	<div class="flex">
+		{#each multipleImages as image, i}
+			<img src={image} alt="carousel post" />
+		{/each}
+	</div>
+{/if}
+
+{#if multipleImages.length === 5}
+	<div class="container grid grid-cols-6 gap-1">
+		{#each multipleImages as image, i}
+			{#if i < 3}
+				<div class="col-span-2">
+					<img class="" src={image} alt="organic post" />
+				</div>
+			{:else}
+				<div class="col-span-3">
+					<img class="w-full h-full object-fit" src={image} alt="organic post" />
+				</div>
+			{/if}
+		{/each}
+	</div>
+{/if}
