8172cc6cf1ba236b7f27cbb88bfd77822d653b2f
Kevin Carmody <kevin@skinofstars.com>
2023-07-13 12:03:09 +0100

    fix: add select style

--- src/routes/pilot/+page.svelte
+++ src/routes/pilot/+page.svelte
@@ -34,7 +34,11 @@
 >
 	<div class="flex mt-2">
 		<p class="px-2">Please select a study:</p>
-		<select bind:value={selected} on:change={setId}>
+		<select
+			bind:value={selected}
+			on:change={setId}
+			class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+		>
 			{#each pilotOptions as pilot}
 				<option value={pilot} label={pilot.name} />
 			{/each}
